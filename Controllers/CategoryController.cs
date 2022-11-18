099df9078ba868cf2da8925097f9c13d4dba562b
pikh1 <pikh1@LAPTOP-2LAUS2MF>
2022-11-18 12:03:03 +0200

    implement ability for admins to CRUD Categories

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalePortal.Data;
+using SalePortal.Entities;
+
+namespace SalePortal.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly SalePortalDbConnection _context;
+
+        public CategoryController(SalePortalDbConnection context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var categoryEntity = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoryEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryEntity);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] CategoryEntity categoryEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoryEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryEntity);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var categoryEntity = await _context.Categories.FindAsync(id);
+            if (categoryEntity == null)
+            {
+                return NotFound();
+            }
+            return View(categoryEntity);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] CategoryEntity categoryEntity)
+        {
+            if (id != categoryEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoryEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryEntityExists(categoryEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryEntity);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var categoryEntity = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoryEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryEntity);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'SalePortalDbConnection.Categories'  is null.");
+            }
+            var categoryEntity = await _context.Categories.FindAsync(id);
+            if (categoryEntity != null)
+            {
+                _context.Categories.Remove(categoryEntity);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryEntityExists(int id)
+        {
+          return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+}
