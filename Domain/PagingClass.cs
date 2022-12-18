d298cfcb0d5a97e30b8ed8af7f8ebaedebe977dc
pikh1 <pikh1@LAPTOP-2LAUS2MF>
2022-12-18 12:42:06 +0200

    Added PagingClass

---
+++ Domain/PagingClass.cs
@@ -0,0 +1,24 @@
+﻿namespace CryptoCurrencies.Application.Domain
+{
+    public class PagingClass<T> : List<T>
+    {
+        public int PageIndex { get; set; }
+        public int TotalPage { get; set; }
+        public PagingClass(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPage = (int) Math.Ceiling(count/(double)pageSize);
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPage;
+
+        public static PagingClass<T> Create(List<T> sourse, int pageIndex, int pageSize)
+        {
+            var count = sourse.Count;
+            var items = sourse.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            return new PagingClass<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
